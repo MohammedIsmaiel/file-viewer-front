d8cf41823344428edb898dc4b6769c3bde543545
mohammedismaiel <mohammedismaiel333.z@gmail.com>
2024-06-01 23:39:22 +0300

    added app config with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectoryComponent } from './component/directory/directory.component';
+import { FileComponent } from './component/file/file.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/directory', pathMatch: 'full' },
+  { path: 'directory', component: DirectoryComponent },
+  { path: 'file', component: FileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectoryComponent } from './component/directory/directory.component';
+import { FileComponent } from './component/file/file.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { basicAuthInterceptor } from './interceptor/basic-auth.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DirectoryComponent, FileComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideHttpClient(withInterceptors([basicAuthInterceptor]))],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
